5269c800e88820ee31e68d2141805b747c7b3776
fstdp <fstdp@hotmail.com>
2013-12-09 15:58:40 +0100

    Update and rename sumo.cpp to Sumo.cpp

--- Sumo.cpp
+++ Sumo.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
-#include "sumo.h"
+#include "Sumo.h"
 
-sumo::sumo(){
+Sumo::Sumo(){
   pinMode(7,OUTPUT);
   pinMode(8,OUTPUT);
   pinMode(9,OUTPUT);
   pinMode(10,OUTPUT);
 }
 
-void sumo::forward(int l, int r, int t)
+void Sumo::forward(int l, int r, int t)
 {
   analogWrite(9,l);
   analogWrite(10,r);
@@ -17,7 +17,7 @@ void sumo::forward(int l, int r, int t)
   delay(t);
 }
 
-void sumo::reverse(int l, int r, int t)
+void Sumo::reverse(int l, int r, int t)
 {
   analogWrite(9,l);
   analogWrite(10,r);
@@ -26,14 +26,14 @@ void sumo::reverse(int l, int r, int t)
   delay(t);
 }
 
-void sumo::brake(int t)
+void Sumo::brake(int t)
 {
   analogWrite(9,0);
   analogWrite(10,0);
   delay(t);
 }
 
-void sumo::rotate(int t)
+void Sumo::rotate(int t)
 {
   analogWrite(9,100);
   analogWrite(10,100);
@@ -48,17 +48,3 @@ void sumo::rotate(int t)
   }
   delay(abs(t));
 }
-
-    Status
-    API
-    Training
-    Shop
-    Blog
-    About
-
-    © 2013 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Contact
-
